32588b4d9776f6426a9d872c6875fc67bfdf740d
Umut Çalışkan <umutcaliskann37@gmail.com>
2025-01-09 01:26:07 +0300

    update

--- app/(tabs)/home.tsx
+++ app/(tabs)/home.tsx
@@ -1,14 +1,25 @@
-import { View, Text, SafeAreaView } from 'react-native'
-import React from 'react'
+import React, { useEffect, useState } from 'react';
+import { View, Text, SafeAreaView } from 'react-native';
+import { getAuth, User } from 'firebase/auth';
 
-const HomeScreen = () => {
+const HomeScreen: React.FC = () => {
+  const [userEmail, setUserEmail] = useState<string | null>('');
+
+  useEffect(() => {
+    const auth = getAuth();
+    const user: User | null = auth.currentUser;
+    if (user) {
+      setUserEmail(user.email);
+    }
+  }, []);
+
   return (
-    <SafeAreaView className='flex-1'>
-      <View className='flex-1 items-center justify-center'>
-      <Text>home</Text>
-      </View>    
+    <SafeAreaView className="flex-1">
+      <View className="flex-1 items-center justify-center">
+        <Text>Hoşgeldiniz , {userEmail}</Text>
+      </View>
     </SafeAreaView>
-  )
-}
+  );
+};
 
-export default HomeScreen
+export default HomeScreen;
