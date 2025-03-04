141781d423d99d6d591cf66acdbcf4b19ce10cf4
Umut Çalışkan <umutcaliskan@Mac.bbrouter>
2025-03-04 04:30:19 +0300

    Firebase Security Secured

--- firebaseConfig.js
+++ firebaseConfig.js
@@ -3,16 +3,7 @@ import AsyncStorage from '@react-native-async-storage/async-storage';
 import firebase from 'firebase/compat/app';
 import 'firebase/compat/auth';
 import 'firebase/compat/firestore'; // Firestore'u içe aktar
-
-const firebaseConfig = {
-    apiKey: "AIzaSyCykOQkK07hUbrhiFfl6J8ma7cCb1H4ipQ",
-    authDomain: "resumebuilder-picksoso.firebaseapp.com",
-    projectId: "resumebuilder-picksoso",
-    storageBucket: "resumebuilder-picksoso.firebasestorage.app",
-    messagingSenderId: "934278391962",
-    appId: "1:934278391962:web:6068955921992528ac5723",
-    measurementId: "G-X6Z55TCGQK"
-};
+import { firebaseConfig } from './config';
 
 let app;
 let isConnected = true;
